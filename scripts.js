f8372e88d40a1eb3ce705b97b406c50a4c52a266
CAusmann <90360421+CAusmann@users.noreply.github.com>
2024-03-04 12:46:36 -0600

    commit

--- scripts.js
+++ scripts.js
@@ -1,28 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Initialize the loader
   var loaderWrapper = document.querySelector(".loader-wrapper");
 
-  // Check if all content (including images, stylesheets, etc.) is loaded
   window.addEventListener("load", function () {
-    // Hide the loader after everything is loaded
     fadeOutLoader(loaderWrapper);
   });
 
-  // Simulate a delay for demonstration purposes (adjust as needed)
   setTimeout(function () {
-    // Hide the loader after the delay, even if not all content is loaded
     fadeOutLoader(loaderWrapper);
-  }, 3000); // 3000 milliseconds (3 seconds) delay, adjust as needed
+  }, 3000);
 });
 
 function fadeOutLoader(loaderWrapper) {
-  // Add the "fade-out" class to trigger the opacity transition
   loaderWrapper.classList.add("fade-out");
 
-  // After the animation duration, hide the loader
   setTimeout(function () {
     loaderWrapper.style.display = "none";
-  }, 500); // Adjust the time to match your CSS animation duration
+  }, 500);
 }
 
 
